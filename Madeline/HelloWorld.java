615ec51d4784a12239dfa50556ea2714ed002d00
maddscarr <42410035+maddscarr@users.noreply.github.com>
2018-08-15 08:49:06 -0400

    Add files via upload

---
+++ Madeline/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+Madeline Carroll
+8/13/18
+This program will print out "Hello World."
+*/
+public class HelloWorld {
+
+public static void main (String args[]) {
+System.out.println("   ____ ");
+System.out.println("   / \\");
+System.out.println("  /---\\+ ");
+System.out.println(" /     \\ ");
+System.out.println("/       \\ ");
+}
+
+}
