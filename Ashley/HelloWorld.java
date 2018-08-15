c9009480c8f30e916d9a6760d5dbef564d7f91bd
aamichaelis <42378850+aamichaelis@users.noreply.github.com>
2018-08-15 08:38:21 -0400

    Add files via upload

---
+++ Ashley/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+Ashley
+8/13/2018
+This program will print out "Bishop Ireton."
+*/
+
+public class HelloWorld {
+
+   public static void main(String args[])  {
+        System.out.println(" -  -  ");
+        System.out.println("\\___/");
+                 }
+              
+ }
