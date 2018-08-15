bb3a2f63beff145b73070b583b0c5d23e5127c1b
maddscarr <42410035+maddscarr@users.noreply.github.com>
2018-08-15 08:47:35 -0400

    Add files via upload

---
+++ Madeline/OldMacDonald.java
@@ -0,0 +1,23 @@
+/*Madeline Carroll */
+
+public class OldMacDonald {
+
+ 
+
+
+
+
+public static void main (String[] args) { 
+System.out.println(5/10);
+System.out.println(1 / 2 * 2 * 10);
+System.out.println(1.0 / 2 * 10);
+System.out.println(1 / 2.0 * 10);
+System.out.println(13 % 5);
+}
+
+
+public static void getVerse (String animal, String sound){
+System.out.println("Old MacDonald had a farm \n EIEIO \n And on that farm he had a "+animal+ "\nEIEIO"+ "With a "+sound+ sound+ "Here and a " + sound+ sound+ "There \n Here a "+sound+ "There a "+sound+ "\n Everywhere a "+sound+ sound+ "Old MacDonald had a farm \nEIEIO");
+}
+
+}
