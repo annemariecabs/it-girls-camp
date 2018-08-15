d7d5537552d4c25bdafc87af39f88dc16e6c0a7f
Caroline <42378797+CarolineGMiller@users.noreply.github.com>
2018-08-15 08:36:44 -0400

    Add files via upload

---
+++ Caroline/Caroline.java
@@ -0,0 +1,15 @@
+public class Caroline {
+   public static void main (String args []) {
+      printVerse(" Ring-ding-ding-ding-dingeringeding, Gering-ding-ding-ding-dingeringeding, Gering-ding-ding-ding-dingeringeding!");      printVerse( " ");
+      printVerse( " ");
+      
+      }
+      
+      public static void printVerse (String word) {
+      System.out.println("Dog goes Cat goes Bird goes tweet And mouse goes squeekCow goes moo Frog goes croak And the elephant goes toot");
+      System.out.println("Ducks say quack and fish go blub and the seal goes ow ow ow But there's one sound that no one knows");   
+      System.out.println("What does the fox say?" +word+ " What does the fox say");
+      System.out.println("What does the fox say?" +word+ " What does the fox say");
+   }
+
+}
---
+++ Caroline/methods.java
@@ -0,0 +1,12 @@
+public class methods {
+
+public static int multiply(int num1, int num2) {
+return num1 * num2;
+} 
+ public static void main (String [] args) { 
+ System.out.println("Multiply returns:" + multiply(12345678, 9876543));
+}
+
+}
+
+
