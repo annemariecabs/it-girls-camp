ead0085f234e82f81cbc0c8008cd5c40e4eae92c
Caroline <42378797+CarolineGMiller@users.noreply.github.com>
2018-08-16 12:28:43 -0400

    Add files via upload

---
+++ Caroline/ChallengeSheet2.java
@@ -0,0 +1,71 @@
+public class ChallengeSheet2 {
+
+   public static void main (String[]args) {
+
+String s="01010101";
+String s2= s.replace("0,1");
+
+System.out.println(s);
+
+
+
+
+
+
+// String s="Miller, Caroline";
+// 
+// String s2= s.replace(",","");
+// 
+// int k=s.indexOf(" ");
+// String first=s.substring(k+1);
+// String last=s.substring(0, k-1);
+// 
+// 
+// System.out.println(first+" "+last);
+// 
+
+
+
+
+
+
+
+
+
+ //  String s="703-343-3810";
+//      String s2= s.replace("-","");
+//      System.out.println(s2);
+//      
+
+     
+      // boolean findAsterisk;
+//      findAsterisk=string1("*");
+//      System.out.println(findAsterisk);
+//      boolean findFirstAsterisk;
+//      findFirstAsterisk=string1("*");
+//      System.out.println(findFirstAsterisk);
+//       }
+//     
+//      public static boolean string1(String s) {
+//      if(s.length() >0 && (s.lastIndexOf("*")==s.length()-1))
+//      return true;
+//      
+//   else
+//   return false ;  
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+   
