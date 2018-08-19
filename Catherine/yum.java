c408f81d33e0c4b52daad07bc5ff347d8cd51863
annemariecabs <caballeroa1@bishopireton.org>
2018-08-19 14:10:33 -0400

    Add files via upload

---
+++ Catherine/yum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class yum {
+public static void main 
+System.out.print("Type in a double");
+double sausage = reader.nextDouble();
+double cheese = return Double (sausage);
+System.out.print(cheese);
+}
+public static double return Double (double pep)}
+return pep * 2;
+}
+
+  
